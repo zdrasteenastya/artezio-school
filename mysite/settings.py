5a9d3ac49befaae958d786d40cb5bbc95809976c
zdrasteenastya <anastasia.provotorova@artezio.com>
2018-08-17 21:44:05 +0300

    Fix review remarks

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'snowpenguin.django.recaptcha2',
+    # 'snowpenguin.django.recaptcha2',
     'littleform'
 ]
 
