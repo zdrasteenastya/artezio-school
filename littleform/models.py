4bfe91ed2ca5a313e993bcb725736104c296c447
artezio_anastasiap <anastasia.provotorova@artezio.com>
2018-08-23 14:14:06 +0300

    Add verbose name

--- littleform/models.py
+++ littleform/models.py
@@ -4,17 +4,17 @@ from django.db import models
 
 
 class Junior(models.Model):
-    username = models.CharField(max_length=200)
-    phone_number = models.CharField(max_length=12)
-    email = models.CharField(max_length=50)
-    link_sns = models.CharField(max_length=200)
-    education = models.CharField(max_length=200)
-    english = models.CharField(max_length=200)
-    languages = models.TextField()
-    experience = models.TextField()
-    link_code = models.TextField()
-    agreement = models.BooleanField(default=False)
-    file = models.FileField(upload_to='user_files/')
+    username = models.CharField(max_length=200, verbose_name=u"ФИО")
+    phone_number = models.CharField(max_length=12, verbose_name=u"Номер телефона")
+    email = models.CharField(max_length=50, verbose_name=u"E-mail")
+    link_sns = models.CharField(max_length=200, verbose_name=u"Ссылка на профиль в соц сетях")
+    education = models.CharField(max_length=200, verbose_name=u"Образование")
+    english = models.CharField(max_length=200, verbose_name=u"Уровень английского языка")
+    languages = models.TextField(verbose_name=u"Языки программирования")
+    experience = models.TextField(verbose_name=u"опыт ")
+    link_code = models.TextField(verbose_name=u"Ссылка на ваш публичный репозиторий")
+    agreement = models.BooleanField(default=False, verbose_name=u"Cогласен на обработку персональных данных")
+    file = models.FileField(upload_to='user_files/', verbose_name=u"Файл")
 
     def __str__(self):
         return self.username.encode('utf-8')
